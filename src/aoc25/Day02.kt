15ef1b1dbdef3897b894ed636f8a3a2c63ed633c
Jan Bína <binajohny@gmail.com>
2025-12-02 17:52:58 +0100

    day 2 faster

--- src/aoc25/Day02.kt
+++ src/aoc25/Day02.kt
@@ -1,8 +1,8 @@
 package aoc25
 
 import Day
+import utils.digitCount
 import java.io.BufferedReader
-import kotlin.math.abs
 
 class Day02(
     input: BufferedReader,
@@ -16,29 +16,38 @@ class Day02(
         }
 
     override fun part1(): Long {
-        fun Long.isInvalid(): Boolean {
-            val s = this.toString()
-            if (s.length % 2 != 0) return false
-            for (i in 0 until s.length / 2) {
-                if (s[i] != s[s.length / 2 + i]) {
-                    return false
-                }
+        return ranges.sumOf { range ->
+            range.sumOf { num ->
+                if (num.toString().isInvalid(numOfChunks = 2)) num else 0
             }
-            return true
         }
-
-        return ranges.sumOf { range -> range.sumOf { num -> if (num.isInvalid()) num else 0} }
     }
 
     override fun part2(): Long {
-        fun Long.isInvalid(): Boolean {
-            val s = this.toString()
-            return (2..s.length).any { numOfChunks ->
-                s.length % numOfChunks == 0
-                        && s.chunked(s.length / numOfChunks).toSet().size == 1
+        fun String.isInvalid(): Boolean {
+            return (2..length).any { numOfChunks ->
+                isInvalid(numOfChunks)
             }
         }
 
-        return ranges.sumOf { range -> range.sumOf { num -> if (num.isInvalid()) num else 0} }
+        return ranges.sumOf { range ->
+            range.sumOf { num ->
+                if (num.toString().isInvalid()) num else 0
+            }
+        }
+    }
+
+    private fun String.isInvalid(numOfChunks: Int): Boolean {
+        if (length % numOfChunks != 0) return false
+
+        val digitsPerChunk = length / numOfChunks
+
+        for (i in 0..<digitsPerChunk) {
+            for (testChunk in 1..<numOfChunks) {
+                if (get(i) != get(i + testChunk * digitsPerChunk)) return false
+            }
+        }
+
+        return true
     }
 }
